4abdcbbf7f0ae041398b868ed8e1079f6a62e75f
And4Web <anands2807@gmail.com>
2022-06-21 15:06:28 +0530

    initiated

---
+++ 1. Chapter 1/01_hello.py
@@ -0,0 +1,8 @@
+'''
+Author: Harry
+Licenced to: ABC Company
+***********Thanks for reading**********
+'''
+import os # importing the os module 
+
+print("this is my first python program.")
---
+++ 1. Chapter 1/03_pr_03_module_usage.py
@@ -0,0 +1,3 @@
+from playsound import playsound
+# playsound('D:\\MyData\\Business\\code playground\\Python Course With Notes\\1. Chapter 1\\play.mp3')
+playsound('E:\\youtube courses\\CWH Python\\python-programs\\1. Chapter 1\\play.mp3')
