538f027d0f21e576eb226bf117c74eea68d6f81d
And4Web <anands2807@gmail.com>
2022-06-24 11:58:11 +0530

    chapter 5 added

---
+++ 5. Chapter 5/01_dictionary_syntax.py
@@ -0,0 +1,12 @@
+myDict = {
+    "Fast": "In a Quick Manner",
+    "Anand": "A Coder",
+    "Marks": [1, 2, 5],
+    "anotherdict": {'anand': 'Player'}
+}
+
+# print(myDict['Fast'])
+# print(myDict['Anand'])
+myDict['Marks'] = [45, 78]
+print(myDict['Marks'])
+print(myDict['anotherdict']['anand'])
